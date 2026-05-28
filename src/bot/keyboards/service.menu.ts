0a26c3ee367411568f11bfad1cbe69ec7c467a5d
Gino Ayyoubian <129810895+gino-ayyoubian@users.noreply.github.com>
2026-05-29 01:33:28 +0330

    Add bot components: keyboards, middlewares, commands, handlers

---
+++ src/bot/keyboards/service.menu.ts
@@ -0,0 +1,8 @@
+import { Markup } from 'telegraf';
+
+export const serviceMenuKeyboard = Markup.inlineKeyboard([
+  [Markup.button.callback('سرویس A', 'service:a')],
+  [Markup.button.callback('سرویس B', 'service:b')],
+  [Markup.button.callback('بازگشت', 'nav:back')],
+  [Markup.button.callback('لغو', 'nav:cancel')],
+]);
